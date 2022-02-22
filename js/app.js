6e96905af59031096a879173df6b8a7d0e81b81a
Deceit-87 <manuelfreund318@gmail.com>
2022-02-22 17:36:10 +0100

    aggiunta stampa

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log('ciao')
+
 // Descrizione:
 // Visualizzare in pagina 5 numeri casuali. 
 // ( con un alert )
@@ -38,13 +38,14 @@ setTimeout(userAnswer, 3000);
 
 
 function userAnswer(userNum){
+    const ARRAY_USER_NUM = [];
     let i = 1
     do{
        
        const userNum = parseInt(prompt(`'inserisci uno ad uno i numeri che hai visto!numero ${[i]}'`))
        if (!isNaN(userNum)){
         i++ 
-     
+        ARRAY_USER_NUM.push(userNum)
         } 
        else {
         parseInt(prompt(`'inserisci uno ad uno i numeri che hai visto!numero ${[i]}'`));
@@ -52,14 +53,43 @@ function userAnswer(userNum){
        }
 
        console.log(userNum);
+       console.log(ARRAY_NUMERI_RDM)
     }
 
     while(i < 6 )
 
+    console.log(ARRAY_USER_NUM)
+    const ARRAY_NUMERI_TRUE = [];
+
+    for (let i = 0; i < ARRAY_USER_NUM.length; i++) {
+        if (ARRAY_NUMERI_RDM.indexOf(ARRAY_USER_NUM[i]) >= 0){
+
+            ARRAY_NUMERI_TRUE.push(ARRAY_USER_NUM[i])
+
+        }
+        
+    }
+            console.log(ARRAY_NUMERI_TRUE);
 
 
+       
+    if ((ARRAY_NUMERI_TRUE.length > 1) && (ARRAY_NUMERI_TRUE.length < ARRAY_NUMERI_RDM.length))
+            DASHBOARD.append(`Ti sei ricordato ${ARRAY_NUMERI_TRUE.length} numeri su ${ARRAY_NUMERI_RDM.length}!
+            ${ARRAY_NUMERI_TRUE}`)
 
-    
+    else if (ARRAY_NUMERI_TRUE.length = ARRAY_NUMERI_RDM.length){
+        DASHBOARD.append(`Ti sei ricordato tutti i numeri!
+            ${ARRAY_NUMERI_TRUE}`)
+    }
+
+    else if (ARRAY_NUMERI_TRUE.length = 1){
+        DASHBOARD.append(`Ti sei ricordato un solo numero!!
+            ${ARRAY_NUMERI_TRUE}`)
+    }
+
+    else {
+        DASHBOARD.append('Ti ricordi almeno come ti chiami ? ')
+    }
 }
 
 
